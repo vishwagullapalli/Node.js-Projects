fcba51e18df416ef25a1a167366601244d7c784a
Vishwa Gullapalli <73298181+vishwagullapalli@users.noreply.github.com>
2026-01-30 22:01:18 +0200

    Added /menu and /hours endpoints

---
+++ restaurant-web-server/data/operatingHours.js
@@ -0,0 +1,14 @@
+export default {
+    defaultHours: {
+        open: 11,
+        closed: 22,
+    },
+    monday: {
+        open: null,
+        closed: null,
+    },
+    sunday: {
+        open: 12,
+        closed: 20,
+    },
+};
--- restaurant-web-server/index.js
+++ restaurant-web-server/index.js
@@ -1,10 +1,20 @@
 import Fastify from "fastify";
+import operatingHours from "./data/operatingHours.js";
+import menuItems from "./data/menuItems.js";
 
 const app = Fastify();
 const port = 3000;
 
 app.get("/", async (request, response) => {
     return "Welcome to What's Fare is Fair!";
+});
+
+app.get("/menu", async (request, response) => {
+    response.send(menuItems);
+});
+
+app.get("/hours", async (request, response) => {
+    response.send(operatingHours);
 });
 
 await app.listen({ port });
