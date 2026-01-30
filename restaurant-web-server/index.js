d17f8db886aef9c8724f34b333720cb58a147f01
Vishwa Gullapalli <73298181+vishwagullapalli@users.noreply.github.com>
2026-01-31 00:57:05 +0200

    Updated endpoints with ejs

--- restaurant-web-server/index.js
+++ restaurant-web-server/index.js
@@ -1,21 +1,40 @@
 import Fastify from "fastify";
 import operatingHours from "./data/operatingHours.js";
 import menuItems from "./data/menuItems.js";
+import ejs from "ejs";
+import fastifyView from "@fastify/view";
 
 const app = Fastify();
 const port = 3000;
 
+app.register(fastifyView, {
+    engine: {
+        ejs: ejs,
+    },
+});
+
 app.get("/", async (request, response) => {
-    return "Welcome to What's Fare is Fair!";
+    return response.view("views/index.ejs", { name: "What's Fare is Fair " });
 });
 
 app.get("/menu", async (request, response) => {
-    response.send(menuItems);
+    return response.view("views/menu.ejs", { menuItems });
 });
 
 app.get("/hours", async (request, response) => {
-    response.send(operatingHours);
+    const days = [
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+        "sunday",
+    ];
+    return response.view("views/hours.ejs", { operatingHours, days });
 });
 
-await app.listen({ port });
-console.log(`Web Server is listening at http://localhost:${port}`);
+app.listen({ port }, (err, address) => {
+    if (err) throw err;
+    console.log(`Server running at ${address}`);
+});
