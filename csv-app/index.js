7e8738b88e08e6f3daee0c76c10b8f268b758a4c
Vishwa Gullapalli <73298181+vishwagullapalli@users.noreply.github.com>
2026-01-30 12:03:48 +0200

    Added Person class

--- csv-app/index.js
+++ csv-app/index.js
@@ -1,5 +1,6 @@
 import { writeFileSync } from "fs";
 import { createInterface } from "readline";
+import { appendFileSync } from "fs";
 
 const content = "Test content!";
 
@@ -15,21 +16,38 @@ const readline = createInterface({
     output: process.stdout,
 });
 
-// const readLineAsync = (message) =>
-//     new Promise((resolve) => readline.question(message, resolve));
+const readLineAsync = (message) =>
+    new Promise((resolve) => readline.question(message, resolve));
 
-// Promisify is used to convert traditional callback based functions into promise based functions. Example usage for readLineAsync functions
+// // Promisify is used to convert traditional callback based functions into promise based functions. Example usage for readLineAsync function is
 
-import { promisify } from "util";
+// import { promisify } from "util";
 
-const readLineAsync = promisify(readline.question).bind(readline);
-(async () => {
-    try {
-        const name = await readLineAsync("What is your name? ");
-        console.log(`Hello, ${name}!`);
-    } catch (err) {
-        console.log("Error:", err.message);
-    } finally {
-        readline.close();
+// const readLineAsync = promisify(readline.question).bind(readline);
+// (async () => {
+//     try {
+//         const name = await readLineAsync("What is your name? ");
+//         console.log(`Hello, ${name}!`);
+//     } catch (err) {
+//         console.log("Error:", err.message);
+//     } finally {
+//         readline.close();
+//     }
+// })();
+
+class Person {
+    constructor(name = "", number = "", email = "") {
+        this.name = name;
+        this.number = number;
+        this.email = email;
     }
-})();
+    saveToCSV() {
+        const content = `${this.name},${this.number},${thgis.email}\n`;
+        try {
+            appendFileSync("./contacts.csv", content);
+            console.log(`${this.name} Saved!`);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
