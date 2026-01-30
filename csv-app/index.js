20c6e79106e65adc92cb6995d1a6954cf797a698
Vishwa Gullapalli <73298181+vishwagullapalli@users.noreply.github.com>
2026-01-30 11:56:23 +0200

    Using Promisify to wrap synchronous function

--- csv-app/index.js
+++ csv-app/index.js
@@ -15,5 +15,21 @@ const readline = createInterface({
     output: process.stdout,
 });
 
-const readLineAsync = (message) =>
-    new Promise((resolve) => readline.question(message, resolve));
+// const readLineAsync = (message) =>
+//     new Promise((resolve) => readline.question(message, resolve));
+
+// Promisify is used to convert traditional callback based functions into promise based functions. Example usage for readLineAsync functions
+
+import { promisify } from "util";
+
+const readLineAsync = promisify(readline.question).bind(readline);
+(async () => {
+    try {
+        const name = await readLineAsync("What is your name? ");
+        console.log(`Hello, ${name}!`);
+    } catch (err) {
+        console.log("Error:", err.message);
+    } finally {
+        readline.close();
+    }
+})();
